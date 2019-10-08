5b15f1f31c01085b25921cfccfdc3dfec8dd1f1f
whiskpo0 <whiskypo082@gmail.com>
2019-10-08 14:30:14 -0500

    Implement authorization using Gates

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -66,6 +66,9 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        if(\Gate::denies('update-question', $question)){ 
+            abort(403, 'Accesss denied'); 
+        }
         return view('questions.edit', compact('question')); 
     }
 
@@ -78,6 +81,10 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        if(\Gate::denies('update-question', $question)){ 
+            abort(403, 'Accesss denied'); 
+        }
+
         $question->update($request->only('title', 'body')); 
 
         return redirect()->route('questions.index')->with('success', 'Your question has been updated!');  
@@ -91,6 +98,10 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        if(\Gate::denies('delete-question', $question)){ 
+            abort(403, 'Accesss denied'); 
+        }
+
         $question->delete(); 
         
         return redirect()->route('questions.index')->with('success', 'Your question has been deleted!');
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,13 +36,17 @@
                                 <a href=" {{ $question->url }}">{{$question->title}}</a>
                             </h3>
                             <div class="ml-auto">
-                                <a href="{{ route('questions.edit', $question->id)  }} " class="btn btn-sm btn-outline-info">Edit</a>    
+                                @if (Auth::user()->can('update-question', $question))
+                                    <a href="{{ route('questions.edit', $question->id)  }} " class="btn btn-sm btn-outline-info">Edit</a>                                        
+                                @endif
+                                @if (Auth::user()->can('delete-question', $question))
+                                <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="post">
+                                    @method('DELETE')
+                                    @csrf
+                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>                       
+                                @endif
                             </div>     
-                            <form class="form-delete ml-1" action="{{ route('questions.destroy', $question->id) }}" method="post">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>                       
                         </div>
                         <p class="lead">
                             Asked by 
