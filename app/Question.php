1b29c16f35f27cdb28bcb6679237e7a7f40691a3
whiskpo0 <whiskypo082@gmail.com>
2019-10-08 11:26:47 -0500

    Add votes, answers and views counter to question item

--- app/Question.php
+++ app/Question.php
@@ -14,19 +14,34 @@ class Question extends Model
         return $this->belongsTo(User::class); 
     }
 
+    // Gets the title attribute and makes it into a slug
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value; 
         $this->attributes['slug'] = Str::slug($value); 
     }
 
+    // Set the url for questions
     public function getUrlAttribute()
     {
         return route("questions.show", $this->id); 
     }
 
+    // Displays date question was posted 
     public function getCreatedDateAttribute()
     {
         return $this->created_at->diffForHumans(); 
+    }
+
+    //Checks to see if the answer has been answered
+    public function getStatusAttribute()
+    {
+        if($this->answers > 0){ 
+            if($this->best_answer_id){ 
+                return "answered-accepted"; 
+            }
+            return "answered"; 
+        }
+        return "unanswered"; 
     }
 }
