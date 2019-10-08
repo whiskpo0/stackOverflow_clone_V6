4d1e664b4547e52064ab2987689ffdab2a741864
whiskpo0 <whiskypo082@gmail.com>
2019-10-08 15:39:17 -0500

    Generate fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -21,4 +21,16 @@ class Answer extends Model
     {
         return  \Parsedown::instance()->text($this->body); 
     }
+
+    public static function boot()
+    {
+        parent::boot(); 
+
+        static::created(function($answer){ 
+            $answer->question->increment('answers_count');
+            
+        }); 
+
+       
+    }
 }
