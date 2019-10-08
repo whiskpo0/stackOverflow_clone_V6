5bff29d623c4ad2a2545f3cd369b7eda3da4a813
whiskpo0 <whiskypo082@gmail.com>
2019-10-08 14:50:19 -0500

    Implement authorization using policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,10 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct(){ 
+
+        $this->middleware('auth', ['except' => ['index', 'show']]); 
+    }
     /**
      * Display a listing of the resource.
      *
@@ -66,6 +70,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question); 
+
         return view('questions.edit', compact('question')); 
     }
 
@@ -78,6 +84,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
+
         $question->update($request->only('title', 'body')); 
 
         return redirect()->route('questions.index')->with('success', 'Your question has been updated!');  
@@ -91,6 +99,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         $question->delete(); 
         
         return redirect()->route('questions.index')->with('success', 'Your question has been deleted!');
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -36,13 +36,18 @@
                                 <a href=" {{ $question->url }}">{{$question->title}}</a>
                             </h3>
                             <div class="ml-auto">
+                                @can ('update', $question)
+                                    
                                 <a href="{{ route('questions.edit', $question->id)  }} " class="btn btn-sm btn-outline-info">Edit</a>    
-                            </div>     
-                            <form class="form-delete ml-1" action="{{ route('questions.destroy', $question->id) }}" method="post">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>                       
+                                @endcan
+                                @can('delete', $question)
+                                <form class="form-delete ml-1" action="{{ route('questions.destroy', $question->id) }}" method="post">
+                                        @method('DELETE')
+                                        @csrf
+                                    <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                </form>                       
+                                @endcan
+                            </div> 
                         </div>
                         <p class="lead">
                             Asked by 
