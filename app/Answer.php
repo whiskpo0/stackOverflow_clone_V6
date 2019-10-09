725a52cbda81d030a616b8e601acb19b2b43f4e2
whiskpo0 <whiskypo082@gmail.com>
2019-10-09 12:26:12 -0500

    Implement delete functionality for answer

--- app/Answer.php
+++ app/Answer.php
@@ -35,8 +35,11 @@ class Answer extends Model
         parent::boot(); 
 
         static::created(function($answer){ 
-            $answer->question->increment('answers_count');
-            
+            $answer->question->increment('answers_count');            
+        }); 
+        
+        static::deleted(function($answer){ 
+            $answer->question->decrement('answers_count');            
         }); 
     }
 }
