3dc6da96199ca0b8f4fa2d65d8f089c06d506ff3
whiskpo0 <whiskypo082@gmail.com>
2019-10-09 12:38:23 -0500

    Update best_answer_id on questions table when answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -39,7 +39,17 @@ class Answer extends Model
         }); 
         
         static::deleted(function($answer){ 
-            $answer->question->decrement('answers_count');            
+            $question = $answer->question; 
+            $question->decrement('answers_count');            
+            if($question->best_answer_id === $answer->id){ 
+                $question->best_answer_id = NULL; 
+                $question->save(); 
+            }
         }); 
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : ''; 
     }
 }
