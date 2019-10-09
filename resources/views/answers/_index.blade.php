73f76ecb0d9e178390f706f8a12dff64bb1eac39
whiskpo0 <whiskypo082@gmail.com>
2019-10-09 13:52:11 -0500

    Implement accept the answer as best answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -17,9 +17,12 @@
                             <a title="This answer is not useful" class="vote-down off">
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer" class="{{$answer->status}} mt-2">
+                                <a title="Mark this answer as best answer" class="{{$answer->status}} mt-2" onclick="event.preventDefault(); document.getElementById('accept-answer-{{ $answer->id }}').submit()">
                                 <i class="fas fa-check fa-2x"></i>                                    
                             </a>
+                            <form id="accept-answer-{{ $answer->id }}" action="{{ route('answers.accept', $answer->id) }}" method="POST" style="display:none;">
+                                @csrf                                
+                            </form>
                         </div>
                         <div class="media-body">
                             {!! $answer->body_html !!}
