7b0fbc1c517b56a5428fd48e175fb8aba8480673
whiskpo0 <whiskypo082@gmail.com>
2019-10-10 11:43:08 -0500

    Implement DRY principle on Answer and Question model

--- app/Answer.php
+++ app/Answer.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    use Votabletrait; 
+    
     protected $fillable = ['body', 'user_id']; 
     
     public function question()
@@ -47,18 +49,5 @@ class Answer extends Model
     {
         return $this->id === $this->question->best_answer_id ? 'vote-accepted' : ''; 
     }
-    
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable'); 
-    }
 
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
-    }
 }
